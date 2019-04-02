e660c7bf68e42ab3c9d22746c4792d1be861299e
PascalNguyen98 <48725484+PascalNguyen98@users.noreply.github.com>
2019-04-02 17:58:53 +0200

    Add files via upload
    update gene_pool.py by adding selection and fitness

--- Gene pool.py
+++ Gene pool.py
@@ -1,7 +1,7 @@
-class Individuals:
-    def __init__(self,gen_code,sum,len,):
+class Individual:
+    def __init__(self,gen_code,fitness):
         self.gen_code=gen_code
-        self.sum=sum
+        self.fitness=
         self.len=len
         
     def getFitness(self):
@@ -16,4 +16,13 @@ class Population:
     def __init__(self,ListOfIndividuals):
         self.ListOfIndividuals=ListOfIndividuals
         
+#trier le code genetic
+
+def fitness(List): #écart à 0
+    for individual in   List:
+        individual.fitness=sum(individual.gen_code)
         
+
+def select_individuals(list):
+    list = sorted(list, key=lambda list: list.fitness, reverse=True) #trier les 
+    
