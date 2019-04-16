af40f8ad4c7b1be25116263c25b82abe8d777ed4
EwanSan <48725388+EwanSan@users.noreply.github.com>
2019-04-16 16:21:30 +0200

    Update Gene pool.py

--- Gene pool.py
+++ Gene pool.py
@@ -1,5 +1,6 @@
 import random
 
+
 def tab_int(nom_fichier):
     fichier = open(nom_fichier, "r")
     a=fichier.readline()
@@ -12,12 +13,14 @@ def tab_int(nom_fichier):
     return (nb_of_int,tab)
 
 (nb_of_int, tabdata)=tab_int("small.txt")
-
+tabdata=[5, -2, -3, 7, 4,10,-10,1]
+nb_of_int=8
 class Individual:
     def __init__(self, nb_init):
         self.gen_code=random.sample(tabdata, nb_init)
         self.fitness=-1
         self.length=len(self.gen_code)
+        self.nb_init=nb_init
 
     def getFitness(self):
         return self.fitness
@@ -27,74 +30,92 @@ class Individual:
 
 
 class Population:
-    def __init__(self,nb_of_people):
-        self.ListOfIndividuals = [Individual(4) for _ in range(nb_of_people)]
+    def __init__(self,nb_of_people,nb_int):
+        self.ListOfIndividuals = [Individual(nb_int) for i in range(nb_of_people)]
 
 
-
-#C'est mon test
-def ga():
-    population = Population(100)
-    for individual in population.ListOfIndividuals:
-        print(individual.gen_code)
-        print("\n")
-
-ga()
-        
 #trier le code genetic
 
-listgen=[1,2,3,4,5,6,7,8,9]
-def crossover_individuals(a,b): # melanger le code génétique sans qu'il y ait de répetitions et sans 
-    n=a.getLen()
+def crossover_individuals(list):
+    offsprings=[]
+    for i in range(80):
 
-    if a.gen_code[n//2]>b.gen_code[n//2]:
-        a,b=b,a
-        
-    child = Individual([])
-       
-    split = n//2
-    child.gen_code = a.gen_code[0:split] + b.gen_code[split:n]
+        A=random.sample(list,2)
+        a=A[0]
+        b=A[1]
+        n=a.getLen()
+
+        if a.gen_code[n//2]>b.gen_code[n//2]:
+            a,b=b,a
+
+        child = Individual(a.nb_init)
+
+        split = n//2
+        child.gen_code = a.gen_code[0:split] + b.gen_code[split:n]
+        offsprings.append(child)
+    list=list+offsprings
+    return list
    # population.ListOfIndividuals.append(child)
-    
- 
+
+
 def randomgene(a):
     L=[]
-    for x in listgen:
+    for x in tabdata:
         if x not in a.gen_code:
             L.append(x)
     return random.choice(L)
-    
-    
 
-def mutate(a):
-    if random.uniform(0.0,1.0)<=0.2:
-        a.gen_code[random.randint(1,len(a.gen_code))-1]=randomgene(a)
-        a.gen_code=sorted(a.gen_code)
-    print(a.gen_code)
-    
-    
-    
-        
- 
-    
-    
-    
-    
-    
 
-def fitness(List): #écart à 0 
+
+def mutate(list):
+    for a in list:
+
+        if random.uniform(0.0,1.0)<=0.2:
+            a.gen_code[random.randint(0,len(a.gen_code))-1]=randomgene(a)
+            a.gen_code=sorted(a.gen_code)
+
+    return list
+
+
+
+
+
+
+def fitness(List): #écart à 0
     for individual in   List:
         individual.fitness=sum(individual.gen_code)
-        
+    return List
+
 
 def select_individuals(list):
-    list = sorted(list, key=lambda list: list.fitness, reverse=True) #trier les individuals
-    
-    
-a=Individual([1,2,3,4]) # test 
-b=Individual([5,6,7,8])    
-crossover_individuals(a,b)
-random.choice(listgen)
-randomgene(a)
-mutate(a)
+    #list = sorted(list, key=lambda individual: individual.fitness, reverse=True)
+    list = list[:19]
+    return list
 
+
+
+
+
+
+def ga():
+    best_sol=[]
+    for i in range(2,nb_of_int):
+        population = Population(100,i)
+        for gen in range(1000):
+
+            population.ListOfIndividuals = fitness(population.ListOfIndividuals)
+            population.ListOfIndividuals = select_individuals(population.ListOfIndividuals)
+            population.ListOfIndividuals= crossover_individuals(population.ListOfIndividuals)
+            population.ListOfIndividuals = mutate(population.ListOfIndividuals)
+            population.ListOfIndividuals = fitness(population.ListOfIndividuals)
+
+
+            for individual in population.ListOfIndividuals:
+                if individual.fitness==0:
+                    best_sol=individual.gen_code
+                    print(individual.fitness)
+
+    print(best_sol,sum(best_sol))
+    return best_sol
+
+ga()
